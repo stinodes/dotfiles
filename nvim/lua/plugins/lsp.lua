c378e0154b10782b525b4cbf0fd4760212de808b
stinodes <stijn@stinodes.dev>
2024-11-19 13:59:27 +0100

    fix: re-added tab completion browsing

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -71,6 +71,20 @@ cmp.setup({
     },
     mapping = {
         ["<CR>"] = cmp.mapping.confirm({ select = false }),
+        ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item()
+            else
+                fallback()
+            end
+        end),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item()
+            else
+                fallback()
+            end
+        end)
     },
 })
 
